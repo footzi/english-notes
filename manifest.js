c39a4384fe513b35d2a0690a093f8573ed736b01
Vladislav Manakov <vsmanakov@avito.ru>
2023-09-04 18:36:31 +0400

    Обновляет скрипт деплоя

---
+++ manifest.js
@@ -0,0 +1,22 @@
+export const manifest = {
+  id: '1',
+  scope: '/',
+  name: 'English Notes',
+  display: 'standalone',
+  start_url: '/',
+  short_name: 'English Notes',
+  theme_color: '#FFFFFF',
+  description: 'English Notes',
+  orientation: 'any',
+  background_color: '#FFFFFF',
+  related_applications: [],
+  prefer_related_applications: false,
+  icons: [
+    {
+      src: 'pwa-icon.png',
+      sizes: [96, 128, 192, 256, 384, 512],
+    },
+  ],
+  features: ['Cross Platform', 'fast', 'simple'],
+  categories: ['social'],
+};
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
+import { manifest } from './manifest.js';
 
 export default defineConfig({
-    plugins: [react()],
-})
+  plugins: [react(), VitePWA({ injectRegister: 'inline', manifest })],
+});
