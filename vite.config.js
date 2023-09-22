fff3d7f79a38c2beb618bbff8e515ba7d05f6d97
Vladislav Manakov <vsmanakov@avito.ru>
2023-09-22 10:13:46 +0400

    Деактивирует SW

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa';
-import { manifest } from './manifest.js';
+// import react from '@vitejs/plugin-react';
+// import { VitePWA } from 'vite-plugin-pwa';
+// import { manifest } from './manifest.js';
 
 export default defineConfig({
   base: './',
-  plugins: [react(), VitePWA({ injectRegister: 'inline', manifest, registerType: 'autoUpdate' })],
+  // plugins: [react(), VitePWA({ injectRegister: 'inline', manifest, registerType: 'autoUpdate' })],
 });
