689d86d41d041430539500b857a90807ef591ff1
Vladislav Manakov <vsmanakov@avito.ru>
2023-10-05 10:17:10 +0400

    Добавляет отмену

--- public/service-worker.js
+++ public/service-worker.js
@@ -8,7 +8,7 @@ self.addEventListener('install', (evt) => {
   console.log('[ServiceWorker] Install');
   evt.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      console.log('[ServiceWorker] Pre-caching offline page');
+      // console.log('[ServiceWorker] Pre-caching offline page');
 
       if (Object.keys(cache).length) {
         return cache.addAll(FILES_TO_CACHE);
@@ -26,7 +26,7 @@ self.addEventListener('activate', (evt) => {
       return Promise.all(
         keyList.map((key) => {
           if (key !== CACHE_NAME) {
-            console.log('[ServiceWorker] Removing old cache', key);
+            // console.log('[ServiceWorker] Removing old cache', key);
             return caches.delete(key);
           }
         })
@@ -37,7 +37,7 @@ self.addEventListener('activate', (evt) => {
 });
 // listen for fetch events in page navigation and return anything that has been cached
 self.addEventListener('fetch', (evt) => {
-  console.log('[ServiceWorker] Fetch', evt.request.url);
+  // console.log('[ServiceWorker] Fetch', evt.request.url);
   // when not a navigation event return
   if (evt.request.mode !== 'navigate') {
     return;
--- src/modules/Content/Tasks/index.jsx
+++ src/modules/Content/Tasks/index.jsx
@@ -3,23 +3,51 @@ import { Question } from './Question/index.jsx';
 
 export const Tasks = ({ questions }) => {
   const [filteredQuestions, setQuestions] = useState(questions);
+  const [lastAnsweredQuestion, setLastAnsweredQuestion] = useState(null);
   const containerRef = useRef(null);
 
-  const onOk = (id) => {
-    setQuestions(() => filteredQuestions.filter((item) => item.id !== id));
-
+  const focusElement = () => {
     setTimeout(() => {
       const firstInput = containerRef?.current.querySelectorAll('input')[0];
 
       if (firstInput) {
         firstInput.focus();
       }
-    }, 100);
+    }, 0);
+  };
+
+  const onOk = (id) => {
+    const questions = filteredQuestions.filter((item) => item.id !== id);
+    const question = filteredQuestions.find((item) => item.id === id);
+
+    setQuestions(questions);
+    setLastAnsweredQuestion(question);
+
+    focusElement();
+  };
+
+  const onCancel = () => {
+    const questions = [lastAnsweredQuestion, ...filteredQuestions];
+    setQuestions(questions);
+
+    focusElement();
   };
 
   useEffect(() => {
     setQuestions(questions);
   }, [questions]);
+
+  useEffect(() => {
+    const handleCancel = (event) => {
+      if (event.key === 'Escape' && lastAnsweredQuestion) {
+        onCancel();
+      }
+    };
+
+    containerRef?.current.addEventListener('keydown', handleCancel);
+
+    return () => containerRef?.current.removeEventListener('keydown', handleCancel);
+  }, [containerRef, lastAnsweredQuestion]);
 
   return (
     <div ref={containerRef}>
