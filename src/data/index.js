562665957357db079d8331deb1b848ba87bd75d4
Vladislav Manakov <vsmanakov@avito.ru>
2024-03-02 14:19:36 +0400

    Новые слова

--- src/data/index.js
+++ src/data/index.js
@@ -8,7 +8,6 @@ import NUMBERS from './numbers.json';
 import IRREGULAR_VERBS from './irregular-verbs.json';
 import COMPARATIVES from './comparatives.json';
 import OXFORD_WORDS from './oxford-words-a2-b1.json';
-import SUPERLATIVES from './superlatives.json';
 import QUANTIFIERS from './quantifiers.json';
 import TRAIN_FROM_PADDINGTON_BOOK from './train-from-paddington-book.json';
 import TENSES from './tenses.json';
@@ -26,7 +25,6 @@ const questions = {
   irregularVerbs: IRREGULAR_VERBS,
   comparatives: COMPARATIVES,
   oxfordWords: OXFORD_WORDS,
-  superlatives: SUPERLATIVES,
   quantifiers: QUANTIFIERS,
   trainFromPaddingtonBook: TRAIN_FROM_PADDINGTON_BOOK,
   tenses: TENSES,
--- src/modules/Content/Grammar/index.jsx
+++ src/modules/Content/Grammar/index.jsx
@@ -3,12 +3,26 @@ import cn from 'classnames';
 import styles from './index.module.css';
 
 export const Grammar = ({ grammar }) => {
-  const { src, layout } = grammar;
+  const { src, layout, images } = grammar;
 
-  if (!src?.length) {
+  if (!src?.length && !images?.length) {
     return <h1>Пока пусто</h1>;
   }
 
+  if (images?.length) {
+    return (
+      <>
+        {images.map((item) => {
+          const cnImage = cn(styles.image, {
+            [styles['image_vertical']]: item.layout === 'vertical',
+          });
+          return <img key={item.src} className={cnImage} src={item.src} alt="" />;
+        })}
+      </>
+    );
+  }
+
+  // @todo deprecated
   const cnImage = cn(styles.image, {
     [styles['image_vertical']]: layout === 'vertical',
   });
